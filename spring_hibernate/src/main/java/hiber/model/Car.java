a0b0315118d97bac1d42be8af68315f6c05d99d4
Andrey Zubkov <zubkoff.id@gmail.com>
2021-03-15 16:37:21 +0300

    Init commit

---
+++ spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,55 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                ", user=" + user +
+                '}';
+    }
+}
